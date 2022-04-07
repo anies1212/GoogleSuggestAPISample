9f742f28fdb2a656afdefcd4579eec585ec59fd2
anies1212 <all.for.me1212sna@gmail.com>
2022-04-07 15:11:29 +0900

    Initial Commit

---
+++ googleSuggestAPISample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  googleSuggestAPISample
+//
+//  Created by anies1212 on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
